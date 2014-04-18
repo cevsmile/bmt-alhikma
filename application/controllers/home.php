fa24064259f5535c8e7795ef5f301a2121b760cb
Lalu Sefty Junaedi <warior.cakep@gmail.com>
2014-04-18 18:33:24 +0800

    Spacing
    Spacing aligment

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,27 +1,28 @@
 <?php
 
 /**
- * 
+ *
  */
 class Home extends CI_Controller {
-	
+
 	function __construct() {
 		parent::__construct();
-		$this->load->model('musers');
+		$this -> load -> model('musers');
 	}
-	
-	function index(){
-		$this->load->view('teskombo');
+
+	function index() {
+		$this -> load -> view('teskombo');
 	}
-	
-	function read(){
-		echo json_encode($this->musers->getAll());
+
+	function read() {
+		echo json_encode($this -> musers -> getAll());
 	}
+
 	function create() {
-		if( !empty( $_POST ) ) {
-			echo $this->musers->create();
+		if (!empty($_POST)) {
+			echo $this -> musers -> create();
 			//echo 'New user created successfully!';
 		}
-	}	
-	
+	}
+
 }
--- application/models/musers.php
+++ application/models/musers.php
@@ -1,17 +1,17 @@
 <?php
 
 /**
- * 
+ *
  */
 class Musers extends CI_Model {
-	
-	function getAll(){
-		$query = $this->db->get('users');
-		if($query->num_rows()>0){
-			return $query->result();
-		}else{
+
+	function getAll() {
+		$query = $this -> db -> get('users');
+		if ($query -> num_rows() > 0) {
+			return $query -> result();
+		} else {
 			return array();
 		}
 	}
-	
+
 }
