36cded82203c8e24abf8e6a28acca8aa65b643c2
Lalu Sefty Junaedi <warior.cakep@gmail.com>
2014-04-20 16:16:58 +0800

    modifying my new menu

--- application/controllers/site.php
+++ application/controllers/site.php
@@ -1,34 +1,30 @@
 <?php
 
-class Site extends CI_Controller{
+class Site extends CI_Controller {
 
 	function index() {
-		$data['title'] = 'Wellcome';		
-		$data['main_content'] = 'public/wellcome';		
-		$this->load->view('public/temp/template', $data);
-	}
-	
-	function validasi(){
-		$this->load->model('user_model');
-		$query = $this->user_model->check_user();
-		
-		if($query) // jika user tervalidasi...
-		{
-			$data = array(
-				'nama' => $this->input->post('nama'),
-				'approved' => true
-			);
-			
-			$this->session->set_userdata($data); //save data to session
-			redirect('members_area/members_area'); // redirect user to members area page.
-		}
-		else 
-		{
-			$this->index(); // redirect user bact to index function above.
-		}
-		
+		$data['title'] = 'Wellcome';
+		$data['main_content'] = 'public/wellcome';
+		$this -> load -> view('public/temp/template', $data);
 	}
 
-	
+	function validasi() {
+		$this -> load -> model('user_model');
+		$query = $this -> user_model -> check_user();
+
+		if ($query)// jika user tervalidasi...
+		{
+			$data = array('Username' => $this -> input -> post('Username'), 'approved' => true);
+
+			$this -> session -> set_userdata($data);
+			//save data to session
+			redirect('members_area/members_area');
+			// redirect user to members area page.
+		} else {
+			$this -> index();
+			// redirect user bact to index function above.
+		}
+
+	}
+
 }
-	
--- application/views/public/temp/menu_list.php
+++ application/views/public/temp/menu_list.php
@@ -1,9 +1,15 @@
-<?php echo form_open('site/validasi');
-$nama_anda = array('name' => 'nama', 'type' => 'text', 'value' => 'admin');
-echo form_input($nama_anda);
-$pass_anda = array('name' => 'password', 'type' => 'password', 'value' => 'admin');
-echo form_password($pass_anda);
-$subkan = array('type' => 'submit', 'name' => 'submit', 'value' => 'Masuk', 'class' => 'ui-button-primary ui-button ui-widget ui-state-default ui-corner-all ui-button-text-only');
-echo form_submit($subkan);
-echo form_close();
+<?php
+//open login form
+	echo form_open('site/validasi');
+		$username = array('name' => 'username', 'type' => 'text', 'value' => 'admin');
+		echo form_input($username);
+		
+		$password = array('name' => 'password', 'type' => 'password', 'value' => 'admin');
+		echo form_password($password);
+	
+		$subkan = array('type' => 'submit', 'name' => 'submit', 'value' => 'Login');
+		echo form_submit($subkan);
+		
+	echo form_close();
+//close login form
 ?>
